5277d2d0ffee6dd5325c3b07bd18d40c07358b13
SaiMasram750 <saimasram54@gmail.com>
2025-04-03 21:58:18 +0530

    Madlibs
    Beginner project

---
+++ Madlibs.py
@@ -0,0 +1,9 @@
+#Making Madlibs in python
+
+Name=input("Enter Your name")
+adjective1=input("Enter adjective1")
+adjective2=input("Enter adjective2")
+profession=input("Enter profession you like")
+verb1=input("Enter Verb1")
+verb2=input("Enter Verb2")
+print(f'Hello {Name},you are so {adjective1} and you are gonna become {adjective2} {profession} so don\'t {verb1} you should {verb2} yourself')
